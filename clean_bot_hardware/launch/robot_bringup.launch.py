54325e26bc0823b8a671d56fffeb1862ec236459
Eitan Ori <eitan.ori@mail.huji.ac.il>
2026-01-12 10:26:37 +0200

    I added a test for the lidar

--- clean_bot_hardware/launch/robot_bringup.launch.py
+++ clean_bot_hardware/launch/robot_bringup.launch.py
@@ -132,7 +132,7 @@ def generate_launch_description():
                 'frame_id': 'laser',
                 'inverted': False,
                 'angle_compensate': True,
-                'scan_mode': 'Sensitivity',
+                'scan_mode': '', # Auto-detect scans
             }]
         ),
 
---
+++ clean_bot_hardware/launch/test_lidar.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='sllidar_ros2',
+            executable='sllidar_node',
+            name='sllidar_node',
+            output='screen',
+            parameters=[{
+                'channel_type': 'serial',
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,
+                'frame_id': 'laser',
+                'inverted': False,
+                'angle_compensate': True,
+                'scan_mode': '',  # Auto-detect mode
+            }]
+        )
+    ])
